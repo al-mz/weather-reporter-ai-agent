3055da32535f79a81e828513511ba0e0b8ebb07f
al-mz <alimhmzadeh@gmail.com>
2025-01-15 21:47:02 -0500

    Add AgentState import and implement start function for workflow initialization

--- src/main.py
+++ src/main.py
@@ -1,1 +1,6 @@
 from langgraph.graph import END, StateGraph
+from graph.state import AgentState
+
+def start(state: AgentState):
+    """Initialize the workflow with the input message."""
+    return state
